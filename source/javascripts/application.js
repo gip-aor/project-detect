f00dc9083ab1698d8d69e737a9953865a8fb6d44
Oscar Maybon <oscar.maybon@gmail.com>
2019-11-08 12:23:50 +0100

    slider + cards

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -46,3 +46,49 @@ $(document).ready(function() {
 jQuery(document).ready(function(){
 jQuery("#mapsvg").mapSvg({markerLastID: 3,width: 596.41547,height: 584.5448,colors: {baseDefault: "#000000",background: "transparent",directory: "#fafafa",status: {},base: "#00597B",stroke: "white",hover: "#143f55",selected: "#143f55"},regions: {'FR-CE': {disabled: false},'FR-LP': {disabled: false}},viewBox: [0,0,596.41547,584.5448],cursor: "pointer",popovers: {mode: "off",on: false,priority: "local",position: "top",centerOn: false,width: 300,maxWidth: 50,maxHeight: 50,resetViewboxOnClose: true,mobileFullscreen: true},gauge: {on: false,labels: {low: "low",high: "high"},colors: {lowRGB: {r: 85,g: 0,b: 0,a: 1},highRGB: {r: 238,g: 0,b: 0,a: 1},low: "#550000",high: "#ee0000",diffRGB: {r: 153,g: 0,b: 0,a: 0}},min: 0,max: false},source: "https://gist.githack.com/Osque/6e3be098f257c7f547def541e078147c/raw/03d7c88126639a47e6c76de13aba5bb6e92dacc2/france-new.svg",title: "France-new",markers: [{id: "marker_1",attached: true,isLink: false,tooltip: "Htkvzevzr",data: {},src: "/mapsvg/markers/pin1_blue.png",width: 15,height: 24,x: 184.64758384204754,y: 337.236670360992,geoCoords: [45.289005,-0.170821]},{id: "marker_2",attached: true,isLink: false,data: {},src: "/mapsvg/markers/pin1_red.png",width: 15,height: 24,x: 329.0707704268872,y: 341.0348287570356,geoCoords: [45.224646,3.305676]},{id: "marker_3",attached: true,isLink: false,data: {},src: "/mapsvg/markers/pin1_yellow.png",width: 15,height: 24,x: 270.21582445088393,y: 452.0433533153723,geoCoords: [43.311423,1.888943]}],responsive: true});
 });
+
+
+       var slide_index = 1;
+        displaySlides(slide_index);
+
+        function nextSlide(n) {
+            displaySlides(slide_index += n);
+        }
+
+        function currentSlide(n) {
+            displaySlides(slide_index = n);
+        }
+
+        function displaySlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("showSlide");
+            if (n > slides.length) { slide_index = 1 }
+            if (n < 1) { slide_index = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[slide_index - 1].style.display = "block";
+        }
+
+
+       var slide2_index = 1;
+        displaySlides2(slide2_index);
+
+        function nextSlide2(n) {
+            displaySlides2(slide2_index += n);
+        }
+
+        function currentSlide2(n) {
+            displaySlides2(slide2_index = n);
+        }
+
+        function displaySlides2(n) {
+            var i;
+            var slides2 = document.getElementsByClassName("showSlide2");
+            if (n > slides2.length) { slide2_index = 1 }
+            if (n < 1) { slide2_index = slides2.length }
+            for (i = 0; i < slides2.length; i++) {
+                slides2[i].style.display = "none";
+            }
+            slides2[slide2_index - 1].style.display = "block";
+        }
