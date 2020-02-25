c120e8b00658f8ab1c0fe4a0479cea17fdc5ae94
Oscar Maybon <oscar.maybon@gmail.com>
2020-02-25 14:22:44 +0100

    analytics

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem "nokogiri", ">= 1.10.4"
 gem 'font-awesome-sass', '~> 5.11.2'
 gem 'middleman-protect-emails'
 gem 'middleman-minify-html'
-gem 'middleman-google-analytics', '~> 3.0'
+gem 'middleman-google-analytics'
 
 
 # Fix critical github warning
--- config.rb
+++ config.rb
@@ -24,11 +24,11 @@ configure :build do
   set :relative_links, true
 end
 
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-159048510-1' # Replace with your property ID.
+end
+
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
-end
-
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-159048510-1' # Replace with your property ID.
 end
